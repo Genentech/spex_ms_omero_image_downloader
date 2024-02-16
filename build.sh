970d37560450d3ad0b201150b80501610e939ffc
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-02-16 16:08:52 +0700

    fixed lib problem

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -f ./microservices/ms-omero-image-downloader/Dockerfile -t spex.omero.image.downloader:latest .
-docker tag spex.omero.image.downloader:latest ghcr.io/genentech/spex.omero.image.downloader:latest
-docker push ghcr.io/genentech/spex.omero.image.downloader:latest
